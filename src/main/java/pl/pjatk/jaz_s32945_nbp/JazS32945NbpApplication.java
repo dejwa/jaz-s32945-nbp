438b473928a3a96e9fcff7cb16543a267cc1f304
dejwa <dawid.kubanek.31@gmail.com>
2026-06-20 15:13:17 +0200

    Initial commit 0.0

---
+++ src/main/java/pl/pjatk/jaz_s32945_nbp/JazS32945NbpApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jaz_s32945_nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS32945NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS32945NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pjatk/jaz_s32945_nbp/JazS32945NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jaz_s32945_nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS32945NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
